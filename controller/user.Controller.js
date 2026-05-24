cd33e782d5034e373e096000b13e11d6640caaec
ak2563718 <ak2563718@gmail.com>
2026-05-24 21:49:03 +0530

    Day 3 Commit

---
+++ controller/user.Controller.js
@@ -0,0 +1,6 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { AppError } from "../utils/AppError.js";
+
+export const userLogin = asyncHandler(async(req, res, next)=>{
+    
+})
--- index.js
+++ index.js
@@ -2,11 +2,18 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
+import { errorMiddleware } from './middleware/error.Middleware.js';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+}))
+app.use(cookieParser());
 
+app.use(errorMiddleware);
 const port = process.env.PORT;
 app.listen(port, ()=>{
     console.log(`server is running at port ${port}`)
